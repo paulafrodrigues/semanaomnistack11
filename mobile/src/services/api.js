93003da20490358b81bba0f640948230308bc07e
Paula Franco <pfr.blackcat@gmail.com>
2020-03-27 21:02:57 -0300

    subindo alterações, e testes

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,5 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://exp://192.168.0.54:3333'
-})
+  baseURL: "http://192.168.0.54:3333"
+});
+
+export default api;
